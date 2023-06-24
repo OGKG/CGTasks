66e4a2bea9da20e4776a787be7ea2585db077f9a
Artyom Fisunenko <artyom.fisunenko@gmail.com>
2023-06-24 16:26:16 +0300

    fix authors in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(name='CGTasks',
-      version='1.1.5',
+      version='1.1.6',
+      author='Vsevolod Hermaniuk & artandfi (Artem Fisunenko)',
+      url='https://github.com/OGKG/CGTasks',
       packages=find_packages(exclude=['CGTasks.tests']),
       install_requires = [
         'MarkLib',
